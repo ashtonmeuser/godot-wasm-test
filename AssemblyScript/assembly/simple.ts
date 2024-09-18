974e7f0f249f24335ed4143765ea5535e4dd87ec
Ashton Meuser <ashtonmeuser@gmail.com>
2024-09-18 09:22:44 -0700

    Add exported count function

--- AssemblyScript/assembly/simple.ts
+++ AssemblyScript/assembly/simple.ts
@@ -1,7 +1,14 @@
 export const global_const: f64 = 1.6180339; // Exported constant
 export let global_mut: i64 = 42; // Exported mutable
+let counter: i64 = 0; // Count invocations
 
-// Exported function
+// Exported add function
 export function add(a: i64, b: i64): i64 {
   return a + b;
+}
+
+// Exported count function
+export function count(): i64 {
+  counter += 1;
+  return counter;
 }
